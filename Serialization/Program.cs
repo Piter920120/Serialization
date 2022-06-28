911b4e0028b5a3f1c6cedc1d66806e0f76075906
Piotr Rynkowski <46453195+Piter920120@users.noreply.github.com>
2022-06-28 12:03:38 +0200

    Update Program.cs

--- Serialization/Program.cs
+++ Serialization/Program.cs
@@ -1,33 +1,20 @@
-﻿using System.Text;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
-using System.IO;
 
 namespace Serialization
 {
     class Program
     {
-        static void Main(string[]args)
+        static void Main(string[] args)
         {
-            // Product Serialization 
-            try
-            {
-                Product product = new Product { Id = "p01", Name =
-                    "Product Name 1", CategoryName = " Category 1", price = new Price { Value = 100, Unit = "USD" }
-                , description = new Description { Color = " red ", Size = "Size 1", Weight = "100 gr" }
-                };
-                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Product));
-                StreamWriter sw = new StreamWriter("product.xml");
-                xmlSerializer.Serialize(sw, product);
-                sw.Close();
-                Console.WriteLine("Serialize");
-            }    
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            Console.ReadLine();
+            Person person1 = new Person();
+            person1.Name = "Piotr";
+            person1.Surname = "Rynkowski";
+            person1.Age = 30;
+            person1.Save("PersonData.xml");
+            Person wczytaj = Person.Load("PersonData.xml");
         }
     }
 }
