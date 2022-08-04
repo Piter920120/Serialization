6e79e01752b42743d445988981546b9810debba7
Piotr Rynkowski <46453195+Piter920120@users.noreply.github.com>
2022-08-04 16:18:21 +0200

    Update Program.cs

--- Serialization/Program.cs
+++ Serialization/Program.cs
@@ -11,8 +11,8 @@ namespace Serialization
         {
             Person person1 = new Person();
             person1.Name = "Piotr";
-            person1.Surname = "Rynkowski";
-            person1.Age = 30;
+            person1.Surname = "Wojtalewicz";
+            person1.Age = 23;
             person1.Save("PersonData.xml");
             Person wczytaj = Person.Load("PersonData.xml");
         }
